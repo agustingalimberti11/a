6bc83390f94f2805a1528d09a34fa02ee36e7b56
agustin <agustingalimberti11@gmail.com>
2025-08-01 11:00:16 -0300

    d

--- conftest.py
+++ conftest.py
@@ -1,30 +1,32 @@
+import os
 import pytest
-import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-# Detectar versión de Selenium
-selenium_version = tuple(int(x) for x in selenium.__version__.split('.') if x.isdigit())
-HAS_SERVICE = selenium_version[0] >= 4
-
-if HAS_SERVICE:
-    from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
+import selenium
 
 @pytest.fixture(scope="function")
 def browser():
     chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")  # Ejecutar sin GUI
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")  # Si querés modo headless
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--start-maximized")
 
-    driver_path = r"C:\Users\PC\Downloads\alkemy\chromedriver.exe"
+    # Detectar versión de Selenium
+    selenium_version = tuple(int(x) for x in selenium.__version__.split('.') if x.isdigit())
+    HAS_SERVICE = selenium_version[0] >= 4 and selenium_version[1] >= 6
 
     if HAS_SERVICE:
+        # Selenium 4.6+ usa selenium-manager
+        driver = webdriver.Chrome(options=chrome_options)
+    else:
+        # Selenium < 4.6 requiere ruta manual al chromedriver
+        driver_path = os.environ.get("CHROMEDRIVER_PATH", r"C:\Users\PC\Downloads\alkemy\chromedriver.exe")
         service = Service(executable_path=driver_path)
         driver = webdriver.Chrome(service=service, options=chrome_options)
-    else:
-        driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
 
     yield driver
     driver.quit()
+
